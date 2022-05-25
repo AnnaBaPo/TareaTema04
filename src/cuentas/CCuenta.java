f3bfa62d62305556f7a257608b4b91421ebe0838
Anna <anna.barcons.portet@gmail.com>
2022-05-25 20:48:58 +0100

    Tercer Commit - añadimos comentarios JavaDoc

--- src/cuentas/CCuenta.java
+++ src/cuentas/CCuenta.java
@@ -1,5 +1,12 @@
 package cuentas;
 
+/**
+ * Está clase sirve para almacener datos de la cuenta bancaria
+ *@author Anna Barcons Portet
+ *@version v1.0
+ *@since 24/05/2022
+ */
+
 public class CCuenta {
 
     /**
@@ -45,6 +52,7 @@ public class CCuenta {
     }
 
     /**
+     * 
      * @return the tipoInterés
      */
     public double getTipoInterés() {
@@ -68,7 +76,13 @@ public class CCuenta {
     public CCuenta()
     {
     }
-
+/**
+ * 
+ * @param nom parámetro que define el nombre de tipo String
+ * @param cue parámetroq que define la cuenta de tipo String
+ * @param sal parámetro que define el saldo de tipo double
+ * @param tipo parámetro que define el tipo de interés de tipo double
+ */
     public CCuenta(String nom, String cue, double sal, double tipo)
     {
         nombre =nom;
@@ -77,18 +91,27 @@ public class CCuenta {
     }
 
     //Metodos 
+    /**
+     * @return saldo
+     */
          public double estado()
     {
         return getSaldo();
     }
-
+/**
+ * @param cantidad
+ * @throws Exception - excepción genéricas
+ */
     public void ingresar(double cantidad) throws Exception
     {
         if (cantidad<0)
             throw new Exception("No se puede ingresar una cantidad negativa");
         setSaldo(getSaldo() + cantidad);
     }
-
+/**
+ * @param cantidad
+ * @throws Exception - - excepción genéricas
+ */
     public void retirar(double cantidad) throws Exception
     {
         if (cantidad <= 0)
